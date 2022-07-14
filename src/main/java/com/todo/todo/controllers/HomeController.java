5baf554be14285b8f5a0f2cf7ab0125a5eb4f5e4
Timur <timazizov@gmail.com>
2022-07-14 11:12:20 +0600

    To Do list

--- src/main/java/com/todo/todo/controllers/HomeController.java
+++ src/main/java/com/todo/todo/controllers/HomeController.java
@@ -1,8 +1,14 @@
 package com.todo.todo.controllers;
 
-import com.todo.todo.DB.DBManager;
+import com.todo.todo.models.Comment;
+import com.todo.todo.models.Folder;
 import com.todo.todo.models.Task;
-import org.springframework.format.annotation.DateTimeFormat;
+import com.todo.todo.models.TaskCategory;
+import com.todo.todo.repos.CategoryRepo;
+import com.todo.todo.repos.CommentRepo;
+import com.todo.todo.repos.FolderRepo;
+import com.todo.todo.repos.TasksRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,61 +17,142 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private FolderRepo folderRepo;
+
+    @Autowired
+    private TasksRepo taskRepo;
+
+    @Autowired
+    private CategoryRepo categoryRepo;
+
+    @Autowired
+    private CommentRepo commentRepo;
+
     @GetMapping(value = "/")
     public String index(Model model){
-        ArrayList<Task> tasks = DBManager.getAllTasks();
-        model.addAttribute("tasks", tasks);
+        List<Folder> folders = folderRepo.findAll();
+        model.addAttribute("folders", folders);
         return "index";
     }
 
+    @GetMapping(value = "/folderdetails/{folderId}")
+    public String getDetails(@PathVariable(name = "folderId") Long id,
+    Model model){
+        Folder folder = folderRepo.getReferenceById(id);
+        List<Task>tasks = taskRepo.findByFolder_Id(id);
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("folder", folder);
+        return "folderdetails";
+    }
+
+    @PostMapping(value = "/addfolder")
+    public String addFolder(@RequestParam(name = "folder_name") String name){
+        Folder folder = new Folder();
+        folder.setName(name);
+        folderRepo.save(folder);
+        return "redirect:/";
+    }
+
     @PostMapping(value = "/addtask")
-    public String addTasks(@RequestParam(name = "task_name") String task_name,
-                           @RequestParam(name = "task_date")
-                           @DateTimeFormat(pattern = "dd.mm.yyyy")
-                           String task_date){
-        Task task = new Task();
-        task.setName(task_name);
-        task.setDeadlineDate(task_date);
-        task.setCompleted(false);
-        DBManager.addTask(task);
+    public String addTask(@RequestParam(name = "task_title") String title,
+                          @RequestParam(name = "task_description") String description,
+                          @RequestParam(name = "folder_id") Long folder_id){
+        Folder folder = folderRepo.getReferenceById(folder_id);
+        if(folder != null){
+            Task task = new Task();
+            task.setTitle(title);
+            task.setDescription(description);
+            task.setStatus(0);
+            task.setFolder(folder);
+            taskRepo.save(task);
+            return "redirect:/folderdetails/" + folder_id;
+        }
+        return "taskCreationError";
+    }
+
+    @PostMapping(value = "/deletefolder")
+    public String deletefolder(@RequestParam(name = "folder_id") Long id){
+        folderRepo.deleteById(id);
         return "redirect:/";
     }
 
-    @GetMapping(value = "/details/{taskId}")
-    public String getDetails(@PathVariable(name = "taskId") Long id,
-                            Model model){
-            Task task = DBManager.getTask(id);
-            model.addAttribute("task", task);
-            return "details";
+    @PostMapping(value = "/addcategory")
+    public String addCategory(@RequestParam(name = "category_name") String name,
+                              @RequestParam(name = "folder_id") Long folder_id){
+        Folder folder = folderRepo.getReferenceById(folder_id);
+        List<TaskCategory> categories = folder.getTaskCategories();
+        if(categories == null)
+            categories = new ArrayList<>();
+        TaskCategory category = new TaskCategory();
+        category.setName(name);
+        categories.add(category);
+        categoryRepo.save(category);
+        folder.setTaskCategories(categories);
+        folderRepo.save(folder);
+        return "redirect:/folderdetails/" + folder_id;
     }
 
-    @PostMapping(value = "/edit")
-    public String edit(@RequestParam(name = "task_id") Long id,
-                       @RequestParam(name = "task_name") String name,
-                       @RequestParam(name = "task_date") String date,
-                       @RequestParam(name = "completed") String completed) {
-        Task task = DBManager.getTask(id);
-        task.setName(name);
-        task.setDeadlineDate(date);
-        boolean isCompleted = false;
-        if (completed.equals("true"))
-            isCompleted = true;
-        task.setCompleted(isCompleted);
-        DBManager.editTask(task);
-
-        return "redirect:/";
+    @PostMapping(value = "/deletecategory")
+    public String deleteCategory(@RequestParam(name = "category_id") Long category_id,
+                                 @RequestParam(name = "folder_id") Long folder_id){
+        Folder folder = folderRepo.getReferenceById(folder_id);
+        TaskCategory category = categoryRepo.getReferenceById(category_id);
+        folder.getTaskCategories().remove(category);
+        folderRepo.save(folder);
+        categoryRepo.deleteById(category_id);
+        return "redirect:/folderdetails/" + folder_id;
     }
 
-    //Decided to use get method because the delete button was inside another form
-    @GetMapping(value = "/delete/{taskId}")
-    public String deleteTask(@PathVariable(name = "taskId") Long id){
-        Task task = DBManager.getTask(id);
-        DBManager.deleteTask(task);
-        return "redirect:/";
+    @PostMapping(value = "/deletetask")
+    public String deleteTask(@RequestParam(name = "task_id") Long task_id){
+        Task task = taskRepo.getReferenceById(task_id);
+        taskRepo.deleteById(task_id);
+        return "redirect:/folderdetails/" + task.getFolder().getId();
     }
 
+    @GetMapping("/details/{taskId}")
+    public String getTaskDetail(@PathVariable(name = "taskId") Long id,
+                                Model model){
+        Task task = taskRepo.getReferenceById(id);
+        List<Comment> comments = commentRepo.findByTask(id);
+        model.addAttribute("comments", comments);
+        model.addAttribute("task", task);
+        return "details";
+    }
+
+    @PostMapping("/edittask")
+    public String editTask(@RequestParam(name = "task_id") Long id,
+                           @RequestParam(name = "task_title") String title,
+                           @RequestParam(name = "task_description") String description,
+                           @RequestParam(name = "status") int status){
+        Task task = taskRepo.getReferenceById(id);
+        task.setTitle(title);
+        task.setDescription(description);
+        task.setStatus(status);
+        Long folder_id = task.getFolder().getId();
+        taskRepo.save(task);
+        return "redirect:/folderdetails/" + folder_id;
+    }
+
+    @PostMapping("/addcomment")
+    public String addComment(@RequestParam(name = "comment") String name,
+                             @RequestParam(name = "task_id") Long task_id){
+        Comment comment = new Comment();
+        comment.setComment(name);
+        Task task = taskRepo.getReferenceById(task_id);
+        comment.setTask(task);
+        commentRepo.save(comment);
+        return "redirect:/details/"+task_id;
+    }
+
+    @GetMapping(value = "/taskCreationError")
+    public String taskCreationError(){
+        return "taskCreationError";
+    }
 }
--- src/main/java/com/todo/todo/models/Task.java
+++ src/main/java/com/todo/todo/models/Task.java
@@ -1,17 +1,48 @@
 package com.todo.todo.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@AllArgsConstructor
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tasks")
 @NoArgsConstructor
-@Data
+@AllArgsConstructor
+@Getter
+@Setter
 public class Task {
-    Long id;
-    String name;
-    String deadlineDate;
-    boolean isCompleted;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String title;
+    private String description;
+    private int status;
 
+    @ManyToOne
+    private Folder folder;
+
+    public static String getStatus(int status){
+        String string = "TO DO";
+        if(status == 1)
+            string = "In Progress";
+        if(status == 2)
+            string = "Done";
+        if(status == 3)
+            string = "Failed";
+
+        return string;
+    }
+
+    public static String getColor(int status){
+        String color = "blue";
+        if(status == 1)
+            color = "orange";
+        if(status == 2)
+            color = "green";
+        if(status == 3)
+            color = "red";
+
+        return color;
+    }
 }
